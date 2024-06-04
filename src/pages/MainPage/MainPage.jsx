faacec77681809e4adcbcf303b00f1c40c78fa23
Crystina D <crystinadang@gmail.com>
2024-06-04 00:21:17 -0400

    Add fonts, partials, and placeholders

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,19 @@
+// import Header from '../../components/Header/Header';
+// import Footer from '../../components/Footer/Footer';
+import "./MainPage.scss";
+
+function MainPage() {
+  return (
+    <div className="main-page">
+      {/* <Header /> */}
+      <div className="main-page__main" >
+        <div className="main-page__main-content" >
+
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </div>
+  )
+}
+
+export default MainPage;
