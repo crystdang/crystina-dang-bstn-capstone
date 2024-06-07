17c24dd506d0517f3c20ae3377dd164aa48e7f4f
Crystina D <crystinadang@gmail.com>
2024-06-07 00:25:50 -0400

    Add additional players to player bar and animate to show hierarchy

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import './Header.scss';
-import Logo from '../../assets/logos/logo-zero.svg';
+import LogoMicro from '../../assets/logos/logo-zero-micro.svg';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header">
       <div className="header__main" >
-        <img className="header__logo" src={Logo} />
+        <img className="header__logo" src={LogoMicro} />
+        <Link to='/future'>
+          <h3 className="header__future">
+            Future
+          </h3>
+        </Link>
       </div>
     </div>
   )
--- src/components/PlayerInfoBar/PlayerInfoBar.jsx
+++ src/components/PlayerInfoBar/PlayerInfoBar.jsx
@@ -1,8 +1,10 @@
 import './PlayerInfoBar.scss';
 import { useState } from 'react';
+import PlayerInfoItem from '../PlayerInfoItem/PlayerInfoItem';
 
 function PlayerInfoBar() {
   const currentTime = Date.now();
+  const [place, setPlace] = useState("4th");
   const [user, setUser] = useState("Vettel");
   const [time, setTime] = useState(currentTime);
   const [team, setTeam] = useState("FERRARI");
@@ -10,18 +12,29 @@ function PlayerInfoBar() {
   return (
     <div className="player-info">
       <div className="player-info__main">
+        <div className="player-info__container player-info__container--other player-info__container--top">
+          <PlayerInfoItem 
+            place="3rd"
+            user="Fernando"
+            time="- 1:20"
+            team="ALPINE"
+          />
+        </div>
         <div className="player-info__container">
-          <h2 className="player-info__user">
-            {user}
-          </h2>
-          <div className="player-info__data">
-            <h2 className="player-info__time">
-              {time}
-            </h2>
-            <h2 className="player-info__team">
-              {team}
-            </h2>
-          </div>
+          <PlayerInfoItem 
+            place={place}
+            user={user}
+            time={time}
+            team={team}
+          />
+        </div>
+        <div className="player-info__container player-info__container--other player-info__container--bottom">
+          <PlayerInfoItem 
+            place="5th"
+            user="Hamilton"
+            time="+ 0:20"
+            team="MERCEDES"
+          />
         </div>
       </div>
     </div>
